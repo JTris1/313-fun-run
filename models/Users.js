22842308eae9097de7439fecbc99868b182837ff
Johnathon Trisler <johnathontrisler@gmail.com>
2022-09-14 14:56:20 -0400

    Added Users table model

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('services/database');
+
+class Users extends Model {}
+
+// Initialize the table connected to the attributes
+Users.init({
+    first_name: {
+        type: DataTypes.STRING
+    },
+    last_name: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    gender: {
+        type: DataTypes.STRING
+    }
+}, {
+    sequelize,
+    modelName: 'users', // THIS IS THE NAME OF THE TABLE IN THE DATABASE
+    timestamps: false       // Ignores CREATEDAT/UPDATEDAT
+});
+
+module.exports = Users;
