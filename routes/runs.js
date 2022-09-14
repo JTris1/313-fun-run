da4b9f41da42f24e07f2db254ca469ecff8d5496
JTris1 <johnathontrisler@gmail.com>
2022-09-14 18:32:29 -0400

    Added all data types to pass to the view

--- routes/runs.js
+++ routes/runs.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const runs = require('models/Run.js');
 const router = express.Router();
 
 // GET available Runs page
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Available Runs' });
+router.get('/', async function(req, res, next) {
+    const run = await runs.findAll();
+    res.render('runs', { 
+        date: run.date,
+        name: run.name,
+        run_type: run.run_type,
+        url: run.url,
+        city: run.city,
+        state: run.state
+    });
 });
 
 module.exports = router;
