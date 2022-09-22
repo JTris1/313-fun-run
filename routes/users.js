4f3cd3d8f1fa2b3b8193d306a8959c2ba687999f
JTris1 <johnathontrisler@gmail.com>
2022-09-21 21:18:37 -0400

    Added individual user and run pages and links

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,23 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   const allUsers = await users.findAll();
-  res.render('users', { 
+  res.render('users/', { 
     title: 'Users',
     users: allUsers
   });
 });
+
+router.get('/:id', async (req, res) => {
+  const user = await users.findOne({
+    where: { id: req.params.id }
+  });
+
+  res.render('users/user', {
+    'first_name': user.first_name,
+    'last_name': user.last_name,
+    'email': user.email,
+    'gender': user.gender
+  })
+})
 
 module.exports = router;
